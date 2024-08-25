5dcc2be73ed7fd12ad00796ef1441c20e018de2d
Sander <schutten@hotmail.com>
2024-08-25 20:57:25 +0000

    working nixdesktop

--- modules/desktop/plasma-desktop.nix
+++ modules/desktop/plasma-desktop.nix
@@ -7,37 +7,37 @@ let
 in
 
 {
-  security.pam.services.kwallet = {
-    name = "kwallet";
-    enableKwallet = true;
-  };
+#  security.pam.services.kwallet = {
+#    name = "kwallet";
+#    enableKwallet = true;
+#  };
 
   services.desktopManager.plasma6 = {
     enable = true;
-    enableQt5Integration = false;
+#    enableQt5Integration = false;
   };
 
-  environment.plasma6.excludePackages = [
-    pkgs.kdePackages.elisa
-    pkgs.kdePackages.khelpcenter
-    pkgs.kdePackages.kwalletmanager
-    pkgs.kdePackages.kwallet
-    pkgs.kdePackages.kinfocenter
-  ];
+#  environment.plasma6.excludePackages = [
+#    pkgs.kdePackages.elisa
+#    pkgs.kdePackages.khelpcenter
+#    pkgs.kdePackages.kwalletmanager
+#    pkgs.kdePackages.kwallet
+#    pkgs.kdePackages.kinfocenter
+#  ];
 
-  services.xserver.enable = false;
+  services.xserver.enable = true;
   services.displayManager.enable = true;
   services.displayManager.sddm = {
     enable = true;
-    wayland.enable = true;
+#    wayland.enable = true;
   };
 
-  services.displayManager.sddm.settings = {
-    Autologin = {
-      Session = "plasma.desktop";
-      User = "system";
-    };
-  };
+#  services.displayManager.sddm.settings = {
+#    Autologin = {
+#      Session = "plasma.desktop";
+#      User = "system";
+#    };
+#  };
 
   environment.systemPackages = with pkgs; [
     (pkgs.vscode-with-extensions.override {
@@ -50,14 +50,14 @@ in
     pkgs.bitwarden-desktop
     pkgs.remmina
     pkgs.kcalc # KDE calculator
-    pkgs.libreoffice-qt6-fresh # Better for Plasma desktop
-    pkgs.hunspell # Spelling used by: LibreOffice
-    pkgs.hunspellDicts.nl_NL # Spelling used by: LibreOffice
+    #pkgs.libreoffice-qt6-fresh # Better for Plasma desktop
+    #pkgs.hunspell # Spelling used by: LibreOffice
+    #pkgs.hunspellDicts.nl_NL # Spelling used by: LibreOffice
     pkgs.pulseaudio    
-    pkgs.kdenlive
-    pkgs.ledger-live-desktop
-    pkgs.gimp
-    pkgs.mqttx
+    #pkgs.kdenlive
+    #pkgs.ledger-live-desktop
+    #pkgs.gimp
+    #pkgs.mqttx
   ];  
   
 
